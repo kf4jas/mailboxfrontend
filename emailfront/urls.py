ae656eafdf7dd102149715eac5c81e2db7241d63
Joe Siwiak <joes@focusschoolsoftware.com>
2021-02-07 13:30:35 -0500

    initial commit

---
+++ emailfront/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+# from django.contrib.auth.decorators import login_required
+from . import views
+app_name = "emailfront"
+urlpatterns = [
+    path("", views.home, name="home"),
+]
